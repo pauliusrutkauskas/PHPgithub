d066bfa44d0056605191ac44ad27313c4584292b
pauliusrutkauskas <paulius.rutkauskas@stud.svako.lt>
2026-01-17 01:47:01 +0200

    Add files via upload
    antra versija

---
+++ phpciklaiv2.php
@@ -0,0 +1,15 @@
+<?php
+$x = 7;
+$iteracijos = 1;
+echo "Pradinė reikšmė: $x<br>";
+while ($x != 1) {
+    if ($x % 2 == 0) {
+        $x = $x / 2;
+    } else {
+        $x = 3 * $x + 1;
+    }
+    echo $x . "<br>";
+    $iteracijos++;
+}
+
+?>
